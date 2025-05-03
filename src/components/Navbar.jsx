0a6fb04f91d0c0a72e773af61548e919af9c0f3f
fsdsfd <tomasmarcos2019@gmail.com>
2025-05-03 16:44:31 -0300

    Modificiaciones al nombre

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark z-3">
   <div className="container-fluid">
-    <Link to='/' className="navbar-brand text-white" onClick={() => window.scrollTo(0, 0)}>Navbar</Link>
+    <Link to='/' className="navbar-brand text-white" onClick={() => window.scrollTo(0, 0)}>Famigas</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
