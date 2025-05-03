b83eac74844b8180d375d1231f702f41196dc2c1
fsdsfd <tomasmarcos2019@gmail.com>
2025-05-03 19:09:16 -0300

    pruebas

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,7 +12,7 @@ const Card = ( { producto } ) => {
     console.log('Agregando el producto al carrito...')
     agregarProductoAlCarritoContext(producto)
   }
-
+  console.log("Foto del producto:", producto.foto);
   return (
 
           <Link  to={`/detalle-producto/${producto.id}`}>
@@ -20,7 +20,7 @@ const Card = ( { producto } ) => {
       <article className="card-total__article">
         <div className="card-total__image-container">
         <img
-  src={Array.isArray(producto.foto) && producto.foto.length > 0 ? producto.foto[0] : 'https://via.placeholder.com/150'}
+  src={Array.isArray(producto.foto) && producto.foto.length > 0 ? producto.foto[0] : 'http://localhost:8080/uploads/b16c0a78-2de2-416c-982b-6e6632b6ba9b.png'}
   alt={producto.nombre || 'Producto sin nombre'}
   className="card-total__image"
 />
